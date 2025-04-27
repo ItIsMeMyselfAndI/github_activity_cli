b115d9494a19b64e66a14585ad5558244c8d6457
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-04-27 18:17:15 +0800

    Remove basic and detailed events access. Add general events access. Update README.md usage and sample output.

--- README.md
+++ README.md
@@ -6,46 +6,21 @@ Provide the GitHub username as an argument when running the CLI.
 
 Basic report:
 ```powershell
-github-activity <username>
-```
-Detailed report:
-```powershell
-github-activity <username> <token>
+python github-activity <username>
 ```
 
-# Sample Basic Output
+# Sample Output
 ```powershell
 ====================================================================
                         Recent Activities      
 ====================================================================
 
 - Followed ItIsMeMyselfAndI/github_activity_cli
-- Pushed new commit(s) to "ItIsMeMyselfAndI/github_activity_cli"
-- Created new repository "ItIsMeMyselfAndI/github_activity_cli" 
-- Changed visibility to public in "ItIsMeMyselfAndI/github_activity_cli"
-- Opened a new issue in "ItIsMeMyselfAndI/github_activity_cli"
-- Starred "ItIsMeMyselfAndI/github_activity_cli"
-- ...
-
-====================================================================
-```
-
-# Sample Detailed Output
-```powershell
-====================================================================
-                        Recent Activities      
-====================================================================
-
-- Followed ItIsMeMyselfAndI/github_activity_cli
-- Pushed 3 commit(s) to "ItIsMeMyselfAndI/github_activity_cli"
-    at 2025-04-15T16:41:24Z
-- Created new repository "ItIsMeMyselfAndI/github_activity_cli" 
-    at 2025-04-15T16:41:24Z
-- Changed visibility to public in "ItIsMeMyselfAndI/github_activity_cli"
-    at 2025-04-15T16:41:24Z
-- Opened a new issue in "ItIsMeMyselfAndI/github_activity_cli"
-    at 2025-04-15T16:41:24Z
-- Starred "ItIsMeMyselfAndI/github_activity_cli"
+- Pushed 3 commit(s) to "ItIsMeMyselfAndI/github_activity_cli" at 2025-04-15T16:41:24Z
+- Created new repository "ItIsMeMyselfAndI/github_activity_cli" at 2025-04-15T16:41:24Z
+- Changed visibility to public in "ItIsMeMyselfAndI/github_activity_cli" at 2025-04-15T16:41:24Z
+- Opened a new issue in "ItIsMeMyselfAndI/github_activity_cli" at 2025-04-15T16:41:24Z
+- Starred "ItIsMeMyselfAndI/github_activity_cli" at 2025-04-15T16:41:24Z
 - ...
 
 ====================================================================
--- github_activity_cli.py
+++ github_activity_cli.py
@@ -6,8 +6,8 @@ WIDTH = os.get_terminal_size().columns
 
 class GithubActivity:
     @staticmethod
-    def getInvalidUsernameMessage(url:str) -> tuple:
-        response = requests.get(url).json()
+    def _requestValidResponse(url:str, headers:dict=None) -> list:
+        response = requests.get(url, headers=headers).json()
         try:
             if "Not Found" in response["message"]:
                 error = "[!] Invalid username.\n\tCheck username and try again."
@@ -15,67 +15,81 @@ class GithubActivity:
                 error = "[!] API rate limit exceeded.\n\tWait and try again later."
             else:
                 error = "[!] Unexpected error occurred.\n\tTry again."
-        except KeyError:
-            return None
+        except TypeError:
+            return response
         message = (
             f"{"="*WIDTH}\n"
             f"\n{error}\n\n"
-            f"{"="*WIDTH}"
+            f"{"="*WIDTH}\n"
         )
-        return message
+        print(message)
+        exit(0)
+
+    @staticmethod
+    def getEvents(url:str) -> list:
+        response = GithubActivity._requestValidResponse(url)
+        events = []
+        for event in response:
+            details = {
+                "repo":event["repo"]["name"], "type":event["type"],
+                "timestamp":event["created_at"]
+            }
+            if event["type"] == "PushEvent":
+                details["size"] = event["payload"]["size"]
+            elif event["type"] in ["CreateEvent", "DeleteEvent"]:
+                details["ref_type"] = event["payload"]["ref_type"]
+            elif event["type"] in ["PullRequestEvent", "IssuesEvent", "MemberEvent"]:
+                details["action"] = event["payload"]["action"]
+            events.append(details)
+        return events
 
     @staticmethod
     def getUsernames(url:str) -> list:
-        response = requests.get(url).json()
-        usernames = [user["login"] for user in response]
-        return usernames
-    
-    @staticmethod
-    def getFullNames(url:str) -> list:
-        response = requests.get(url).json()
-        usernames = [user["full_name"] for user in response]
-        return usernames
-
-    def getIssues(url:str) -> list:
-            response = requests.get(url).json()
-            issues = [{"repo":issue["repo"], "created_at":issue["created_at"] , 
-                 "updated_at":issue["updated_at"]} for issue in response]
-            return issues
+        response = GithubActivity._requestValidResponse(url)
+        usernames = [{"type":"following", "username":user["login"]} for user in response]
+        return None
 
     @staticmethod
-    def getBasicEvents(url:str) -> list:
-        response = requests.get(url + "/events").json()
-        events = []
-        for event in response:
-            events.append({"type":event["type"], "repo":event["repo"]["name"]})
-        return events
-    
-    @staticmethod
-    def _getUserRepos(url:str, headers) -> list:
-        response = requests.get(url, headers=headers).json()
-        repos = [repo["url"] for repo in response]
-        return repos
-    
-    @staticmethod
-    def getDetailedEvents(url:str, token:str) -> list:
-        headers = {"Authorization": f"Bearer {token}"}
-        repos = GithubActivity._getUserRepos(url + "/repos", headers)
-        events = []
-        for repo_url in repos:
-            response = requests.get(repo_url + "/events", headers=headers).json()
-            if len(response) == 0:
-                continue
-            for event in response:
-                details = {
-                    "name":event["repo"]["name"], "type":event["type"],
-                    "timestamp":event["created_at"], "url":event["repo"]["url"]
-                }
-                if event["type"] == "PushEvent":
-                    details["commits"] = event["payload"]["size"]
-                events.append(details)
-        return events
+    def formatActivities(activities:list) -> str:
+        f_activities = []
+        for activity in activities:
+            match activity["type"]:
+                case "PushEvent":
+                    line = f"- Pushed {activity["size"]} commit(s) in \"{activity["repo"]}\""
+                case "PullRequestEvent":
+                    line = f"- {activity["action"].title()} a pull request in \"{activity["repo"]}\""
+                case "PullRequestReviewEvent":
+                    line = f"- Reviewed a pull request in {activity["repo"]}"
+                case "PullRequestReviewCommentEvent":
+                    line = f"- Commented on a pull request review in \"{activity["repo"]}\""
+                case "IssuesEvent":
+                    line = f"- {activity["action"].title()} an issue in \"{activity["repo"]}\""
+                case "IssueCommentEvent":
+                    line = f"- Commented on an issue in \"{activity["repo"]}\""
+                case "ForkEvent":
+                    line = f"- Forked \"{activity["repo"]}\""
+                case "WatchEvent":
+                    line = f"- Starred \"{activity["repo"]}\""
+                case "CreateEvent":
+                    line = f"- Created a new {activity["ref_type"]} \"{activity["repo"]}\""
+                case "DeleteEvent":
+                    line = f"- Deleted a {activity["ref_type"]} in \"{activity["repo"]}\""
+                case "ReleaseEvent":
+                    line = f"- Published a new release in \"{activity["repo"]}\""
+                case "PublicEvent":
+                    line = f"- Changed visibility to public in \"{activity["repo"]}\""
+                case "MemberEvent":
+                    line = f"- {activity["action"].title()} a member in \"{activity["repo"]}\""
+                case "GollumEvent":
+                    line = f"- Edited a wiki page in \"{activity["repo"]}\""
+                case "following":
+                    line = f"- Followed \"{activity["username"]}\""
+            if activity["type"] != "following":
+                line += f" at {activity["timestamp"]}"
+            f_activities.append(line)
+        return "\n\n".join(f_activities)
 
-
+        
 def _isValidArgv(argv:list) -> bool:
     message = (
         f"{"="*WIDTH}\n"
@@ -83,7 +97,7 @@ def _isValidArgv(argv:list) -> bool:
         "\tCheck README.md file.\n\n"
         f"{"="*WIDTH}"
     )
-    if (len(argv) <= 1) or (len(argv) > 3):
+    if (len(argv) <= 1) or (len(argv) > 2):
         print(message)
         return False
     return True
@@ -93,45 +107,28 @@ def main() -> None:
         return
 
     base_url = f"https://api.github.com"
-    username, token = (sys.argv[1], None) if len(sys.argv) == 2 else (sys.argv[1], sys.argv[2])
+    username = (sys.argv[1], None) if len(sys.argv) == 2 else (sys.argv[1], sys.argv[2])
     user_endpoint = f"{base_url}/users/{username}"
-    repo_endpoint = f"{base_url}/repos/{username}"
-    search_url = f"{base_url}/"
 
-    message = GithubActivity.getInvalidUsernameMessage(user_endpoint)
-    if message:
-        print(message)
-        return
-    
+    events = GithubActivity.getEvents(user_endpoint + "/events")
     following = GithubActivity.getUsernames(user_endpoint + "/following")
-    print(following)
-    print()
-    starred = GithubActivity.getFullNames(user_endpoint + "/starred")
-    print(starred)
-    print()
-    watching = GithubActivity.getFullNames(user_endpoint + "/subscriptions")
-    print(watching)
-    print()
-    events = GithubActivity.getBasicEvents(user_endpoint)
-    print(events)
-    print()
-    events = GithubActivity.getDetailedEvents(user_endpoint, token)
-    print(events)
-    print()
 
+    activities = events + following
+    if len(activities) == 0:
+        formatted = "No recent activities as of this moment"
+    else:
+        formatted = GithubActivity.formatActivities(activities)
+
+    display = (
+        f"{"="*WIDTH}\n\n"
+        f"{"Recent Activities".center(WIDTH)}\n\n"
+        f"{"="*WIDTH}\n\n"
+        f"{formatted}\n\n"
+        f"{"="*WIDTH}"
+    )
+    print(display)
 
 if __name__ == "__main__":
     print()
-
-    message = (
-        f"{"="*WIDTH}\n"
-        "\n[!] API rate limit exceeded.\n"
-        "\tTry again later.\n\n"
-        f"{"="*WIDTH}"
-    )
-    # try:
     main()
-    # except TypeError:
-    #     print(message)
-
     print()
